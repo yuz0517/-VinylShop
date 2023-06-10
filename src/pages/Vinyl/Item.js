cce9a30351069c82704940cf6b9bc640e37511e4
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2023-06-10 22:39:17 +0900

    vinyl 메뉴 css 수정

--- src/pages/Vinyl/Item.js
+++ src/pages/Vinyl/Item.js
@@ -1,22 +1,41 @@
-import React from "react";
-import './Item.css'
+import React ,{ useState }from "react";
+import "./Item.css";
 import { useLocation } from "react-router-dom";
 function Item() {
   const { state } = useLocation();
   console.log(state.description);
+  
+  let countryEmoji = "";
+  if(state.country==='Japan'){
+    countryEmoji="🇯🇵 ";
+  }else if(state.country==='South Korea'){
+    countryEmoji="🇰🇷 "
+  }else if(state.country==='China'){
+    countryEmoji="🇨🇳 "
+  }
   return (
     <div className="vinylItem0">
+        <p className="vinylItem0-0-p">
+          {state.artist} ✯ {state.title}
+        </p>
       <div className="vinylItem0-0">
+        
         <img className="vinylItem0-0-0" src={state.img0} />
+
         <div className="vinylItem0-0-1">
-          <p>{state.title}</p>
-          <p>{state.artist}</p>
-          <p>{state.label}</p>
-          <p>{state.format}</p>
-          <p>{state.country}</p>
-          <p>{state.year}</p>
-          <p>{state.genre}</p>
-          <p>{state.price}</p>
+          <p className="p-vinylItem-detail">✧title✧ {state.title}</p>
+          <p className="p-vinylItem-detail">✧artist✧ {state.artist}</p>
+          <p className="p-vinylItem-detail">✧label✧ {state.label}</p>
+          <p className="p-vinylItem-detail">✧format✧ {state.format}</p>
+          <p className="p-vinylItem-detail">✧country✧ {countryEmoji}{state.country}</p>
+          <p className="p-vinylItem-detail">✧year✧ {state.year}</p>
+          <p className="p-vinylItem-detail">
+            ✧genre✧ {state.genre1} {state.genre2}
+          </p>
+          <p className="p-vinylItem-detail">✧condition✧ {state.condition}</p>
+          <p className="p-vinylItem-detail">✧sleeve✧ {state.sleeve}</p>
+
+          <p>✧price✧ {state.price}</p>
           <div className="vinylItem0-0-1-0">
             <button>장바구니</button>
             <button>찜</button>
@@ -27,9 +46,9 @@ function Item() {
       <div className="vinylItem0-1">
         <p className="vinyl">detail</p>
         <div className="vinylItemImgs">
-            <img className="vinylItemimg"src={state.img0}/>
-            <img className="vinylItemimg"src={state.img1}/>
-            <img className="vinylItemimg"src={state.img2}/>
+          <img className="vinylItemimg" src={state.img0} />
+          <img className="vinylItemimg" src={state.img1} />
+          <img className="vinylItemimg" src={state.img2} />
         </div>
         <div className="vinylItemDscrp"></div>
         <p className="p-vinyldescription">{state.description}</p>
--- src/pages/Vinyl/VinylList.js
+++ src/pages/Vinyl/VinylList.js
@@ -1,23 +1,43 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import './VinylList.css'
 function VinylList(props) {
-console.log("데이터터터",props.result_num)
-let navigate = useNavigate();
+  console.log("데이터터터", props.result_num);
+  let navigate = useNavigate();
   if (props.result_num === undefined) {
     return (
       <div>
         <div>등록된 상품이 없습니다.</div>
       </div>
     );
-  }else if(props.result_num !== undefined) {
+  } else if (props.result_num !== undefined) {
     return (
       <div>
         <div className="vinyl-item">
-          <img className="vinyl-img" src={props.item.img0} width="200px" onClick={()=>{
-                    navigate("/vinyl/:id", {state: props.item});  
-                }}/>
-          <p className="vinyl-title">{props.item.title}</p>
-          <p className="vinyl-artist">{props.item.artist}</p>
+          <img
+            className="vinyl-img"
+            src={props.item.img0}
+            width="200px"
+            onClick={() => {
+              navigate("/vinyl/:id", { state: props.item });
+            }}
+          />
+          <p
+            className="vinyl-title"
+            onClick={() => {
+              navigate("/vinyl/:id", { state: props.item });
+            }}
+          >
+            {props.item.title}
+          </p>
+          <p
+            className="vinyl-artist"
+            onClick={() => {
+              navigate("/vinyl/:id", { state: props.item });
+            }}
+          >
+            {props.item.artist}
+          </p>
         </div>
       </div>
     );
