4b0f262d2ca7fc377c02bcff07b15d201310a7b9
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2023-06-25 18:55:38 +0900

    Feat 장바구니 delete 쿼리 적용

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -6,34 +6,72 @@ import { auth } from "../../firebase"; //파베
 function Cart() {
   const { sessionUserid, setIsloggedIn } = useContext(Context);
   const [cartdata, setCartdata] = useState([]);
+  const [tempcartdata, setTempcartdata] = useState([]);
   const logout = async () => {
     await signOut(auth);
     sessionStorage.clear();
     setIsloggedIn(false);
   };
   //let cartdata = "";
-  
+
   useEffect(() => {
-    if (sessionUserid === undefined || sessionUserid === null ||sessionUserid==='') {
+    if (
+      sessionUserid === undefined ||
+      sessionUserid === null ||
+      sessionUserid === ""
+    ) {
       //로그인 하면 db에서 불러와 저장되는 sessionuserid 부분에 아무것도 없다면
       //재로그인창으로 이동
       alert("재로그인 해 주세요");
       logout();
     } else {
-    console.log(sessionUserid)
-      Axios.get("http://localhost:8000/api/cart/getcart", {
-        params: { key: sessionUserid },
-      })
-        .then((res) => {
-          console.log(res.data);
-          setCartdata([...res.data]);
-        })
-        .catch((err) => {
-          console.log(err.message);
-        });
+      console.log("삭제", sessionUserid);
     }
-  },[sessionUserid]);
-  console.log(cartdata[0])
+
+    Axios.get("http://localhost:8000/api/cart/getcart", {
+      params: { key: sessionUserid },
+    })
+      .then((res) => {
+        console.log(res.data);
+        setTempcartdata([...res.data]);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+
+      {
+        tempcartdata.map((item, index) => {
+          let key = item.itemid;
+          console.log("delete 실행", index);
+          Axios.delete("http://localhost:8000/api/cart/initdelete", {
+            data: { id: key },
+          })
+            .then((res) => {
+              console.log(res);
+              console.log("삭제완료");
+            })
+            .catch((err) => {
+              console.log(err.message);
+              console.log(err);
+            });
+  
+          return <div key={index}></div>;
+        });
+      }
+    Axios.get("http://localhost:8000/api/cart/getcart", {
+      params: { key: sessionUserid },
+    })
+      .then((res) => {
+        console.log(res.data);
+        setCartdata([...res.data]);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+
+    //return( <div key={item.itemid}></div>);
+  }, [sessionUserid]);
+  console.log(cartdata);
 
   return (
     <>
@@ -41,14 +79,11 @@ function Cart() {
       <p></p>
       <div>
         {cartdata.map((item) => {
-            
-            return (
-                <div key={item.itemid}>
-                    <p>
-                        {item.title}
-                    </p>
-                </div>
-            );
+          return (
+            <div key={item.itemid}>
+              <p key={item.itemid}>{item.title}</p>
+            </div>
+          );
         })}
       </div>
     </>
