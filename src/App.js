7d5e33daba2bda80eaa14e71f386bdaead2f5276
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2023-10-31 14:43:19 +0900

    feat: Add Cart and Orderdetail routes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Vinyl from "./pages/Vinyl";
 import Navbar from "./components/Navbar";
 import Signup from "./pages/Signup";
 import Board_write from "./pages/Board/Board_write";
-import Board_Mine from "./pages/Board/Board_Mine"
+import Board_Mine from "./pages/Board/Board_Mine";
 import Board_Main from "./pages/Board/Board_Main";
 import Board_View from "./pages/Board/Board_View";
 import Board_Update from "./pages/Board/Board_Update";
@@ -17,40 +17,45 @@ import Search from "./pages/Search/Search";
 import { Context } from "./components/ContextProvider";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Item from "./pages/Vinyl/Item";
+import Cart from "./pages/Cart/Cart";
+import Orderdetail from "./pages/OrderSubmit/Orderdetail";
 
 function App() {
   const [isLoggedIn, setIsloggedIn] = useState(false);
   const [sessionUserid, setSessionUserid] = useState(""); //작성자 등록
-  const [prevPagenation,setPrevpagenation] = useState(1);
+  const [prevPagenation, setPrevpagenation] = useState(1);
   return (
     <>
       <Router>
         <Context.Provider
           value={{
-            isLoggedIn,setIsloggedIn,
-            sessionUserid,setSessionUserid,
-            prevPagenation,setPrevpagenation,
+            isLoggedIn,
+            setIsloggedIn,
+            sessionUserid,
+            setSessionUserid,
+            prevPagenation,
+            setPrevpagenation,
           }}
         >
           <Navbar />
 
           <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/Mypage" element={<Mypage_Main/>}>
-              <Route path="myinfo" element={ <Mypage/>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/Mypage" element={<Mypage_Main />}>
+              <Route path="myinfo" element={<Mypage />} />
             </Route>
-            <Route path="/vinyl" element={<Vinyl/>} >
-              
-            </Route>
-            <Route path="/vinyl/:id" element={<Item/>}/>
-            <Route path="/Signup" element={<Signup/>} />
-            <Route path="/Board" element={<Board_Main/>} />
-            <Route path="/Board-write" element={<Board_write/>} />
-            <Route path="/Board-view/:no" element={<Board_View/>} />
-            <Route path="/Board/update" element={<Board_Update/>}/>
-            <Route path="/Signin" element={<Signin/>} />
-            <Route path="/Search/:no" element={<Search/>} />
-            <Route path="/Search/mine" element={<Board_Mine/>} />
+            <Route path="/vinyl" element={<Vinyl />}></Route>
+            <Route path="/vinyl/:id" element={<Item />} />
+            <Route path="/Signup" element={<Signup />} />
+            <Route path="/Board" element={<Board_Main />} />
+            <Route path="/Board-write" element={<Board_write />} />
+            <Route path="/Board-view/:no" element={<Board_View />} />
+            <Route path="/Board/update" element={<Board_Update />} />
+            <Route path="/Signin" element={<Signin />} />
+            <Route path="/Search/:no" element={<Search />} />
+            <Route path="/Search/mine" element={<Board_Mine />} />
+            <Route path="/cart" element={<Cart/>}/>
+            <Route path="/orderdetail" element={<Orderdetail/>}/>
           </Routes>
         </Context.Provider>
       </Router>
