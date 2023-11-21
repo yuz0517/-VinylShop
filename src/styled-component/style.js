a747060bd0de6d7d84093725e96cc04f45407f30
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2023-11-21 14:33:46 +0900

    refactor: Organize styled components in 'style.js' file

---
+++ src/styled-component/style.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+export const Font15px_bold = styled.div`
+  font-size: 15px;
+  font-weight: bold;
+  margin-bottom: 10px;
+  margin-top: 10px;
+`;
+export const Font14px=styled.div`
+    font-size: 14px;
+    margin-bottom: 5px;
+    margin-top: 5px;
+`;
+export const Font14px_gray=styled.div`
+    font-size: 14px;
+    color: #979797;
+    margin-bottom: 5px;
+    margin-top: 5px;
+`;
+
+export const Font14px_bold=styled.div`
+    font-size: 14px
+    margin-bottom: 5px;
+    margin-top: 5px;
+`;
+export const B = styled.div`
+    padding-left: 1.2em;
+`;
+/* input */
+export const Input_Rect_transparent = styled.input`
+    width: 80%;
+    height: 43px;
+`
+/* select*/
+export const Select_Rect_transparent = styled.select`
+    width: 80%;
+    height: 43px;
+`
+/* 도형 */
+
+export const Cylinder_Gray= styled.div`
+cursor: pointer;
+border: 1px solid #dcdcdc;
+width: 30vh;
+height: 43px;
+font-size: 14px;
+display: flex;
+justify-content: center;
+align-items: center;
+margin-right: 5px;
+margin-left: 5px;
+border-radius: 20px;
+margin-bottom: 5px;
+`
