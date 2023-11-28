3595cd33eef0917d70f129a434b6249f200e4705
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2023-11-28 14:42:22 +0900

    add: add file for delivery address component

---
+++ src/pages/OrderSubmit/DeliveryADD.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+
+export default function DeliveryADD() {
+    const [ADDcnt,setADDcnt] = useState(0)
+
+  return (
+    <div>
+        <div>배송지를 추가해주세요.</div>
+        <div>  배송지를 등록하시면 더욱 더 편리하게 선택하실 수 있습니다. </div>
+        <div>
+            배송지명
+        </div>
+        <div>
+        
+        </div>
+    </div>
+  )
+}
