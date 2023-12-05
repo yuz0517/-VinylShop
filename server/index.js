d4790e8c083de66b2a118c29ed426a543610971d
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2023-12-05 23:58:15 +0900

    feat: Implement SELECT query in DB server file for table retrieval

--- server/index.js
+++ server/index.js
@@ -398,7 +398,23 @@ app.post("/api/cart/insert", (req,res) => {
         }
     });
 });
-
+/* -----------Address------------ */
+app.get("/api/address/getallinfo",(req,res)=>{
+    const key = req.query.key;
+    const sqlQuery = "SELECT * FROM board WHERE user_id Like ?";
+    console.log(sqlQuery,key);
+    db.query(sqlQuery,[key],(err,data)=>{
+        
+        if(!err){ 
+            console.log(data)
+            return res.send(data);
+        } else { 
+           
+            res.send(err);
+   
+        }
+    });
+});
 app.listen(PORT, ()=>{
     console.log(`running on port ${PORT}`);
 });
