702ae5f058b42f02b32b7dbabdabf345e47ada48
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2023-12-06 00:05:59 +0900

    feat: Add code to fetch data from the database server

--- src/pages/OrderSubmit/DeliveryADD.js
+++ src/pages/OrderSubmit/DeliveryADD.js
@@ -1,23 +1,92 @@
-import React, { useEffect, useState, useContext} from 'react'
-import { Context } from '../../components/ContextProvider';
+import React, { useEffect, useState, useContext } from "react";
+import Axios from "axios";
+import { Context } from "../../components/ContextProvider";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase"; //파베
+import Modal from "react-modal";
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
 export default function DeliveryADD() {
-    const [ADDcnt,setADDcnt] = useState(0)
-    const { sessionUserid, setsessionUserid } = useContext(Context);
-    console.log(sessionUserid)
+  const [ADDcnt, setADDcnt] = useState(0);
+  const { sessionUserid, setIsloggedIn } = useContext(Context);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [addressdata, setAddressData] = useState([]);
 
-    const onAddrClick =()=>{
-        
+  console.log(sessionUserid);
+
+  const logout = async () => {
+    await signOut(auth);
+    sessionStorage.clear();
+    setIsloggedIn(false);
+  };
+
+  
+  const onAddrClick = () => {};
+  useEffect(() => {
+    if (
+      sessionUserid === undefined ||
+      sessionUserid === null ||
+      sessionUserid === ""
+    ) {
+      //로그인 하면 db에서 불러와 저장되는 sessionuserid 부분에 아무것도 없다면
+      //재로그인창으로 이동
+      alert("재로그인 해 주세요");
+      logout();
+    } else {
+      console.log("삭제", sessionUserid);
     }
+
+    Axios.get("/api/address/getallinfo", {
+      params: { user: sessionUserid },
+    })
+
+      .then((res) => {
+        setAddressData([...addressdata, ...res.data]);
+        //temp_reward_points = res.data[0].reward_points;
+        //setRewardPoints(temp_reward_points);
+        console.log("addressdata from db ", addressdata);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+
+
+  });
+
+  function openModal() {
+    console.log(modalOpen);
+    setModalOpen(true);
+  }
+  function closeModal() {
+    setModalOpen(false);
+  }
+
   return (
     <div>
-        <div>배송지를 추가해주세요.</div>
-        <div>  배송지를 등록하시면 더욱 더 편리하게 선택하실 수 있습니다. </div>
-        <div>
-            <button onclick={onAddrClick}>배송지 추가하기</button>
-        </div>
-        <div>
-        
-        </div>
+      <div>배송지를 추가해주세요.</div>
+      <div> 배송지를 등록하시면 더욱 더 편리하게 선택하실 수 있습니다. </div>
+      <div>
+        <button onClick={openModal}>배송지 목록</button>
+        <Modal
+          contentLabel="Pop up Message"
+          style={customStyles}
+          isOpen={modalOpen}
+          onRequestClose={closeModal}
+          shouldCloseOnOverlayClick={true}
+        >
+          <h2>모달 오픈</h2>
+          <button onClick={() => setModalOpen(false)}>close</button>
+        </Modal>
+      </div>
+      <div></div>
     </div>
-  )
+  );
 }
