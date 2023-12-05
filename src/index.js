de1a7354dc998f59ef1f30868752b8e31496ba3b
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2023-12-06 00:08:32 +0900

    chore: Remove unnecessary console.log statements

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import Modal from 'react-modal';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,3 +10,5 @@ root.render(
 
     
 );
+
+Modal.setAppElement('#root')
