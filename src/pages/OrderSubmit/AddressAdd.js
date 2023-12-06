4a579abec099df848714b26477cb358827ad39e1
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2023-12-06 23:58:03 +0900

    add: Add addressAdd component

---
+++ src/pages/OrderSubmit/AddressAdd.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+export default function AddressAdd() {
+  return (
+    <>
+      <div>AddressAdd</div>
+      <div>배송지 이름</div>
+    </>
+  );
+}
