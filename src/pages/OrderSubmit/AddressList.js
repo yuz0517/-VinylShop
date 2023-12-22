4017e7131051bc9b63e2e0fb5109b8dfbbf66030
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2023-12-22 12:37:30 +0900

    refactor: Separate delivery address information into components

--- src/pages/OrderSubmit/AddressList.js
+++ src/pages/OrderSubmit/AddressList.js
@@ -1,39 +1,35 @@
-import React,{ useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Axios from "axios";
 import { Context } from "../../components/ContextProvider";
+import AddressItem from "./AddressItem";
 export default function AddressList(props) {
-    const { sessionUserid, setIsloggedIn } = useContext(Context);
-    const [addressdata, setAddressData] = useState([]);
-    const zero = 0;
-    useEffect(()=>{
-        Axios.get("http://localhost:8000/api/address/getallinfo", {
+  const { sessionUserid, setIsloggedIn } = useContext(Context);
+  const [addressdata, setAddressData] = useState([]);
+  useEffect(() => {
+    Axios.get("http://localhost:8000/api/address/getallinfo", {
       params: { key: sessionUserid },
     })
 
       .then((res) => {
-        setAddressData([ ...res.data]);
-        //setAddressData(res.data)
-        //temp_reward_points = res.data[0].reward_points;
-        //setRewardPoints(temp_reward_points);
-        console.log("getnewdata",res.data);
+        setAddressData([...res.data]);
       })
       .catch((err) => {
         console.log(err.message);
       });
-    },[])
-    
+  }, []);
 
   return (
     <div>
+      <div>배송지 목록</div>
       {addressdata &&
         addressdata.map((item, idx) => {
-            console.log(item)
-        return (
-          <>
-            <div>{item.address1}</div>
-          </>
-        );
-      })}
+          return (
+            <div key={idx}>
+              <AddressItem recipient={item.recipient} address1={item.address1} address2={item.address2}
+              phone={item.phone} address_name={item.address_name} is_default={item.is_default}/>
+            </div>
+          );
+        })}
     </div>
   );
 }
