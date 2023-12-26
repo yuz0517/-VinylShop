17ae8e0f6c682606fa6097b93c19963728339f02
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2023-12-26 16:34:50 +0900

    feat: Implement data transmission to AddressList.js

--- src/pages/OrderSubmit/AddressItem.js
+++ src/pages/OrderSubmit/AddressItem.js
@@ -1,10 +1,33 @@
-import React from 'react'
-
+import React,{useState} from "react";
+import Axios from "axios";
 export default function AddressItem(props) {
+  const onSelectClick = () => {
+    console.log('선택 클릭')
+     props.sendDataToItem(props);
+      
+  }
+  const onDeleteClick = () => {
+    Axios.delete("http://localhost:8000/api/address/delete", {
+      data: { id: props.address_id },
+    })
+      .then((res) => {
+        alert("삭제되었습니다.");
+  
+        console.log(res);
+
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
   return (
     <div>
-        <div>{props.address_name}</div>
-        <div>{props.recipient}</div>
+      <div>{props.address_name}</div>
+      <button onClick={onSelectClick}>선택</button>
+      <div>{props.recipient}</div>
+      <div>{props.phone}</div>
+      <div>{props.address1 + " " + props.address2 + "[" + props.postal_code + "]"}</div>
+      <button onClick={onDeleteClick}>삭제</button>
     </div>
-  )
+  );
 }
