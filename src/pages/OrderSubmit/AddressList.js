cd47c8a09d2c888ec309d9cf79fa4f54b5025d83
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2023-12-26 16:35:15 +0900

    feat: Implement data transmission to DeliveryADD.js

--- src/pages/OrderSubmit/AddressList.js
+++ src/pages/OrderSubmit/AddressList.js
@@ -2,9 +2,20 @@ import React, { useContext, useEffect, useState } from "react";
 import Axios from "axios";
 import { Context } from "../../components/ContextProvider";
 import AddressItem from "./AddressItem";
-export default function AddressList(props) {
+export default function AddressList({ sendDataToADD }) {
   const { sessionUserid, setIsloggedIn } = useContext(Context);
   const [addressdata, setAddressData] = useState([]);
+  const [dataFromItem, setDataFromItem] = useState([]);
+  const onSelectClick = () => {
+    //sendDataToADD(dataFromItem)
+  };
+  const handleDataFromItem = (data) => {
+    //setDataFromItem({ ...data });
+    sendDataToADD(data);
+    console.log("list으로전달", data);
+    
+  };
+
   useEffect(() => {
     Axios.get("http://localhost:8000/api/address/getallinfo", {
       params: { key: sessionUserid },
@@ -17,6 +28,9 @@ export default function AddressList(props) {
         console.log(err.message);
       });
   }, []);
+  useEffect(() => {
+    
+  }, [dataFromItem]);
 
   return (
     <div>
@@ -25,8 +39,17 @@ export default function AddressList(props) {
         addressdata.map((item, idx) => {
           return (
             <div key={idx}>
-              <AddressItem recipient={item.recipient} address1={item.address1} address2={item.address2}
-              phone={item.phone} address_name={item.address_name} is_default={item.is_default}/>
+              <AddressItem
+                sendDataToItem={handleDataFromItem}
+                address_id={item.address_id}
+                recipient={item.recipient}
+                address1={item.address1}
+                address2={item.address2}
+                phone={item.phone}
+                postal_code={item.postal_code}
+                address_name={item.address_name}
+                is_default={item.is_default}
+              />
             </div>
           );
         })}
