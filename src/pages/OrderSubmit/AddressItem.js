e941ba364ce9574dc6999e63a7017eb2259e70a3
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2023-12-28 10:09:57 +0900

    Update: Hide JSX items when 'visible' state is false

--- src/pages/OrderSubmit/AddressItem.js
+++ src/pages/OrderSubmit/AddressItem.js
@@ -1,20 +1,19 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Axios from "axios";
 export default function AddressItem(props) {
+  const [visible, setVisible] = useState(true);
   const onSelectClick = () => {
-    console.log('선택 클릭')
-     props.sendDataToItem(props);
-      
-  }
+    console.log("선택 클릭");
+    props.sendDataToItem(props);
+  };
   const onDeleteClick = () => {
     Axios.delete("http://localhost:8000/api/address/delete", {
       data: { id: props.address_id },
     })
       .then((res) => {
         alert("삭제되었습니다.");
-  
+        setVisible(false);
         console.log(res);
-
       })
       .catch((err) => {
         console.log(err.message);
@@ -22,12 +21,25 @@ export default function AddressItem(props) {
   };
   return (
     <div>
-      <div>{props.address_name}</div>
-      <button onClick={onSelectClick}>선택</button>
-      <div>{props.recipient}</div>
-      <div>{props.phone}</div>
-      <div>{props.address1 + " " + props.address2 + "[" + props.postal_code + "]"}</div>
-      <button onClick={onDeleteClick}>삭제</button>
+      {visible ===false? (
+        <></>
+      ) : (
+        <>
+          <div>{props.address_name}</div>
+          <button onClick={onSelectClick}>선택</button>
+          <div>{props.recipient}</div>
+          <div>{props.phone}</div>
+          <div>
+            {props.address1 +
+              " " +
+              props.address2 +
+              "[" +
+              props.postal_code +
+              "]"}
+          </div>
+          <button onClick={onDeleteClick}>삭제</button>
+        </>
+      )}
     </div>
   );
 }
