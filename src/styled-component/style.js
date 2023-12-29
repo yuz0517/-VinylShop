1a0914e12d2db492170352e81ae8fa2faafe93cc
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2023-12-29 09:22:58 +0900

    chore: Add styled-component to encapsulate common styles

--- src/styled-component/style.js
+++ src/styled-component/style.js
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 /* 정렬,레이아웃 */
+export const Fixed = styled.div`
+  position: fixed;
+ 
+`
+export const Scroll = styled.div`
+  overflow-y:auto;
+  max-height: 40vh;
+`
 export const Section = styled.div`
   background-color: white;
   width: auto
@@ -43,7 +51,7 @@ export const Image = styled.img`
 `;
 export const Font15px_bold = styled.div`
   font-size: 15px;
-  font-weight: bold;
+  font-weight: 600;
   margin-bottom: 10px;
   margin-top: 10px;
 `;
@@ -59,10 +67,32 @@ export const Font14px_gray = styled.div`
   margin-top: 5px;
   margin-right: 5px;
 `;
+export const Font14px_darkgray = styled.div`
+  font-size: 15px;
+  color: #545454; 
+  margin-bottom: 5px;
+  margin-top: 5px;
+  margin-right: 5px;
+  font-weight: 400;
+`;
+export const Font14px_black = styled.div`
+  font-size: 15px;
+  margin-bottom: 5px;
+  margin-top: 5px;
+  margin-right: 5px;
+  font-weight: 400;
+`;
+export const Font15px_darkgray = styled.div`
+  font-size: 15px;
+  color: #545454; 
+  margin-bottom: 5px;
+  margin-top: 5px;
+  margin-right: 5px;
+`;
 
 export const Font_bold = styled.div`
     font-size: ${(props)=>props.fontsize};
-    font-weight:bold;
+    font-weight:600;
     margin-bottom: 5px;
     margin-top: 5px;
     color: ${(props)=>props.color};
@@ -75,13 +105,19 @@ export const Input_Rect_transparent = styled.input`
   width: ${(props)=>props.width};
   height: 43px;
   margin-bottom: ${(props)=>props.marginbottom};
+  border: 1px solid #dcdcdc;
+  border-radius: 7px;
+  padding: 7px;
+  padding-left: 10px;
+  padding-right: 10px;
+  font-size:15px;
 `;
 /* select*/
 export const Select_Rect_transparent = styled.select`
   width: 80%;
   height: 43px;
 `;
-/* 도형 */
+/* btn */
 
 export const Cylinder_Gray = styled.div`
   cursor: pointer;
@@ -97,3 +133,41 @@ export const Cylinder_Gray = styled.div`
   border-radius: 20px;
   margin-bottom: 5px;
 `;
+
+export const Radius_btn = styled.div`
+  cursor: pointer;
+  border: ${(props)=>props.border};
+  height: ${(props)=>props.height};
+  width: auto;
+  font-size: ${(props)=>props.font_size};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 18px;
+  margin-bottom: 1px;
+  background: ${(props)=>props.background};
+  
+  padding-right: 10px;
+  padding-left: 10px;
+  padding-top: 1px;
+  font-weight: 600;
+  
+`;
+export const Transparent_btn = styled.div`
+  cursor: pointer;
+  border: ${(props)=>props.border};
+  height: ${(props)=>props.height};
+  width: auto;
+  font-size: ${(props)=>props.font_size};
+  color:${(props)=>props.font_color};
+  display: flex;
+  float:right;
+  margin-left: 5px;
+  margin-right: 5px;
+  background: none;
+  padding-top: 1px;
+  font-weight: 500;
+  
+`;
+
+
