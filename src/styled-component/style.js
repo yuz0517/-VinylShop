7dfaaba505ecc2b134c35de8d7a4fede6c4532a7
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2024-01-02 11:25:09 +0900

    Design: Change css property

--- src/styled-component/style.js
+++ src/styled-component/style.js
@@ -5,55 +5,60 @@ export const Center = styled.div`
 
   display: flex;
   justify-content-center;
-`
+`;
 export const Fixed = styled.div`
   position: fixed;
- 
-`
+`;
 export const Scroll = styled.div`
-  overflow-y:auto;
+  overflow-y: auto;
   max-height: 40vh;
-`
+`;
 export const Section = styled.div`
   background-color: white;
   width: auto
   margin-bottom: 10px;
 `;
-export const Div_all=styled.div`
-background-color: #ffffff;
-width:quto;
-place-content:center;
-flex-direction: column;
-margin: auto;
-display: grid;
+export const Div_all = styled.div`
+  background-color: #ffffff;
+  width: quto;
+  place-content: center;
+  flex-direction: column;
+  margin: auto;
+  display: grid;
+`;
+export const Div_all_flex = styled.div`
+  background-color: #ffffff;
 
-`
-export const Div_all_flex=styled.div`
-background-color: #ffffff;
-width:100%;
-place-content:center;
+  place-content: center;
+  
+  display: flex;
 
+  @media (min-width: 768px) {
+    
+  }
 
-display: flex;
-
-`
+  @media (min-width: 992px) {
+    
+  }
+`;
 export const Div_flex_column = styled.div`
   display: flex;
   flex-direction: column;
-  
-  width: ${(props)=>props.width};
-  margin-right: ${(props)=>props.margin_right};
-  margin-left: ${(props)=>props.margin_left};
+
+  width: ${(props) => props.width};
+  margin-right: ${(props) => props.margin_right};
+  margin-left: ${(props) => props.margin_left};
 `;
 export const Div_flex = styled.div`
   display: flex;
-  justify-content: ${(props)=>props.justifycontent};
-  margin-bottom: ${(props)=>props.marginbottom};
+  justify-content: ${(props) => props.justifycontent};
+  margin-bottom: ${(props) => props.marginbottom};
+  width: ${(props) => props.width};
 `;
 export const Image = styled.img`
-  width: ${(props)=>props.width};
-  height: ${(props)=>props.width};
-  margin-right: ${(props)=>props.margin_right};
+  width: ${(props) => props.width};
+  height: ${(props) => props.width};
+  margin-right: ${(props) => props.margin_right};
 `;
 export const Font15px_bold = styled.div`
   font-size: 15px;
@@ -64,18 +69,18 @@ export const Font15px_bold = styled.div`
 export const Font_plain = styled.div`
   margin-bottom: 5px;
   margin-top: 5px;
-  color: ${(props)=>props.color};
+  color: ${(props) => props.color};
 `;
 export const Font14px_gray = styled.div`
   font-size: 14px;
-  color: #979797; 
+  color: #979797;
   margin-bottom: 5px;
   margin-top: 5px;
   margin-right: 5px;
 `;
 export const Font14px_darkgray = styled.div`
   font-size: 15px;
-  color: #545454; 
+  color: #545454;
   margin-bottom: 5px;
   margin-top: 5px;
   margin-right: 5px;
@@ -90,41 +95,41 @@ export const Font14px_black = styled.div`
 `;
 export const Font15px_darkgray = styled.div`
   font-size: 15px;
-  color: #545454; 
+  color: #545454;
   margin-bottom: 5px;
   margin-top: 5px;
   margin-right: 5px;
 `;
 
 export const Font_bold = styled.div`
-    font-size: ${(props)=>props.fontsize};
-    font-weight:600;
-    margin-bottom: 5px;
-    margin-top: 5px;
-    color: ${(props)=>props.color};
+  font-size: ${(props) => props.fontsize};
+  font-weight: 600;
+  margin-bottom: 5px;
+  margin-top: 5px;
+  color: ${(props) => props.color};
 `;
 export const Font_bold_center = styled.div`
-    font-size: ${(props)=>props.font_size};
-    font-weight:${(props)=>props.font_weight};;
-    margin-bottom: 5px;
-    margin-top: 5px;
-    color: ${(props)=>props.color};
-    text-align: center;
+  font-size: ${(props) => props.font_size};
+  font-weight: ${(props) => props.font_weight};
+  margin-bottom: 5px;
+  margin-top: 5px;
+  color: ${(props) => props.color};
+  text-align: center;
 `;
 export const B = styled.div`
   padding-left: 1.2em;
 `;
 /* input */
 export const Input_Rect_transparent = styled.input`
-  width: ${(props)=>props.width};
+  width: ${(props) => props.width};
   height: 43px;
-  margin-bottom: ${(props)=>props.marginbottom};
+  margin-bottom: ${(props) => props.marginbottom};
   border: 1px solid #dcdcdc;
   border-radius: 7px;
   padding: 7px;
   padding-left: 10px;
   padding-right: 10px;
-  font-size:15px;
+  font-size: 15px;
 `;
 /* select*/
 export const Select_Rect_transparent = styled.select`
@@ -136,7 +141,7 @@ export const Select_Rect_transparent = styled.select`
 export const Cylinder_Gray = styled.div`
   cursor: pointer;
   border: 1px solid #dcdcdc;
-  width: ${(props)=>props.width};
+  width: ${(props) => props.width};
   height: 43px;
   font-size: 14px;
   display: flex;
@@ -146,42 +151,40 @@ export const Cylinder_Gray = styled.div`
   margin-left: 5px;
   border-radius: 20px;
   margin-bottom: 5px;
-  margin-top: ${(props)=>props.margin_top};
+  margin-top: ${(props) => props.margin_top};
 `;
 
 export const Radius_btn = styled.div`
   cursor: pointer;
-  border: ${(props)=>props.border};
-  height: ${(props)=>props.height};
+  border: ${(props) => props.border};
+  height: ${(props) => props.height};
   width: auto;
-  font-size: ${(props)=>props.font_size};
+  font-size: ${(props) => props.font_size};
   display: flex;
   justify-content: center;
   align-items: center;
   border-radius: 18px;
   margin-bottom: 1px;
-  background: ${(props)=>props.background};
-  
+  background: ${(props) => props.background};
+
   padding-right: 10px;
   padding-left: 10px;
   padding-top: 1px;
   font-weight: 600;
-  
 `;
 export const Transparent_btn = styled.div`
   cursor: pointer;
-  border: ${(props)=>props.border};
-  height: ${(props)=>props.height};
+  border: ${(props) => props.border};
+  height: ${(props) => props.height};
   width: auto;
-  font-size: ${(props)=>props.font_size};
-  color:${(props)=>props.font_color};
+  font-size: ${(props) => props.font_size};
+  color: ${(props) => props.font_color};
   display: flex;
-  float:right;
-  margin-top: ${(props)=>props.margin_top};
+  float: right;
+  margin-top: ${(props) => props.margin_top};
   margin-left: 5px;
   margin-right: 5px;
   background: none;
   padding-top: 1px;
   font-weight: 500;
-  
 `;
