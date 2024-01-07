a2c641a6603f46b56e41e936852e1c342d90132a
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2024-01-07 23:40:16 +0900

    Design: Change tags

--- src/pages/OrderSubmit/AddressList.js
+++ src/pages/OrderSubmit/AddressList.js
@@ -2,13 +2,20 @@ import React, { useContext, useEffect, useState } from "react";
 import Axios from "axios";
 import { Context } from "../../components/ContextProvider";
 import AddressItem from "./AddressItem";
-import { Font_bold, Font15px_bold,Scroll,Font14px_gray, Font_bold_center } from "../../styled-component/style";
+import {
+  Font_bold,
+  Font15px_bold,
+  Scroll,
+  Font14px_gray,
+  Font_bold_center,
+} from "../../styled-component/style";
 export default function AddressList({ sendDataToADD }) {
   const { sessionUserid, setIsloggedIn } = useContext(Context);
   const [addressdata, setAddressData] = useState([]);
 
-  const handleDataFromItem = (data) => {//AddressItem.js으로부터 데이터를 받으면
-    sendDataToADD(data);//AddressAdd.js로 그 데이터를 보낸다. 
+  const handleDataFromItem = (data) => {
+    //AddressItem.js으로부터 데이터를 받으면
+    sendDataToADD(data); //AddressAdd.js로 그 데이터를 보낸다.
   };
 
   useEffect(() => {
@@ -26,29 +33,27 @@ export default function AddressList({ sendDataToADD }) {
 
   return (
     <>
-    <div>
-      
+      <div></div>
+      <div>
+        {addressdata &&
+          addressdata.map((item, idx) => {
+            return (
+              <div key={item.address_id}>
+                <AddressItem
+                  sendDataToItem={handleDataFromItem}
+                  address_id={item.address_id}
+                  recipient={item.recipient}
+                  address1={item.address1}
+                  address2={item.address2}
+                  phone={item.phone}
+                  postal_code={item.postal_code}
+                  address_name={item.address_name}
+                  is_default={item.is_default}
+                />
+              </div>
+            );
+          })}
       </div>
-      <Scroll>
-      {addressdata &&
-        addressdata.map((item, idx) => {
-          return (
-            <div key={item.address_id}>
-              <AddressItem
-                sendDataToItem={handleDataFromItem}
-                address_id={item.address_id}
-                recipient={item.recipient}
-                address1={item.address1}
-                address2={item.address2}
-                phone={item.phone}
-                postal_code={item.postal_code}
-                address_name={item.address_name}
-                is_default={item.is_default}
-              />
-            </div>
-          );
-        })}
-    </Scroll>
     </>
   );
 }
