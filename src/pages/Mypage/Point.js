9fe118d59568ce549bd84547499afda1056eca55
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2024-01-10 16:57:08 +0900

    Feat: Add Point component with initial structures

---
+++ src/pages/Mypage/Point.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { IoWalletSharp } from "react-icons/io5";
+import { Div_all } from "../../styled-component/style";
+import PointItem from "./PointItem";
+export default function Point() {
+  return (
+    <Div_all>
+      <div>Point</div>
+      <PointItem />
+    </Div_all>
+  );
+}
