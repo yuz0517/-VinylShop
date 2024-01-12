22fe0beea6367e364f963dc2af100ea847d8f2b7
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2024-01-12 09:38:34 +0900

    Feat: Add Axios request to fetch user points
    This commit Fetch user points from the server using Axios.get and set data  in the 'pointData' state.

--- src/pages/Mypage/Point.js
+++ src/pages/Mypage/Point.js
@@ -1,13 +1,37 @@
-import React from "react";
-
+import React, { useContext, useEffect, useState } from "react";
+import Axios from "axios";
 import { IoWalletSharp } from "react-icons/io5";
 import { Div_all } from "../../styled-component/style";
+import { Context } from "../../components/ContextProvider";
 import PointItem from "./PointItem";
 export default function Point() {
+  const { sessionUserid } = useContext(Context);
+  const [pointData, setPointData] = useState([]);
+  useEffect(() => {
+    Axios.get("http://localhost:8000/api/point/get", {
+      params: { key: sessionUserid },
+    })
+      .then((res) => {
+        setPointData([...res.data]);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+  console.log(pointData)
   return (
     <Div_all>
       <div>Point</div>
-      <PointItem />
+
+      {pointData&&
+        pointData.map((item, idx) => {
+        console.log(item)
+        return (
+          <div key={idx} >
+            <PointItem />
+          </div>
+        );
+      })}
     </Div_all>
   );
 }
