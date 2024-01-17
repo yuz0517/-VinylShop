f82a1d91bb7b0ba0d1bf75f9dd27b645e620c517
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2024-01-17 23:54:14 +0900

    Feat: Chagne tag to styled-component

--- src/pages/Board/Board_Mine.js
+++ src/pages/Board/Board_Mine.js
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { Link, Navigate, useLocation , useNavigate} from "react-router-dom";
+import { Link, Navigate, useLocation, useNavigate } from "react-router-dom";
 
 import Axios from "axios";
 import List from "../Board/Board_List/List";
+import {
+  Div_all,
+  Div_all_table,
+  Font_bold,
+  Font13px_darkgray,
+  ColumnCenter,
+} from "../../styled-component/style";
 function Board_Mine() {
   let navigate = useNavigate();
   const location = useLocation();
@@ -11,51 +18,45 @@ function Board_Mine() {
   //const [currentPath, setCurrentPath] = useState('');
 
   //setCurrentPath(location.pathname);
-  let key = sessionStorage.key(0)
-  console.log(key)
+  let key = sessionStorage.key(0);
+  console.log(key);
 
   let [dbdata, set_dbdata] = useState([]);
-  
-  
+
   //let key = '테스트'; //검색 키워드
   useEffect(() => {
+    console.log("실행됨");
 
-    console.log("실행됨")
-    
-      Axios.get("http://localhost:8000/api/boardsearch/mine", {
-        params: { key: key },
-        
+    Axios.get("http://localhost:8000/api/boardsearch/mine", {
+      params: { key: key },
+    })
+      .then((res) => {
+        set_dbdata([...dbdata, ...res.data]);
       })
-        .then((res) => {
-          set_dbdata([...dbdata, ...res.data]);
-          
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    
-    
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
-  
   return (
-    <>
-      <div> 나의 게시물입니다.</div>
-      <div>총 {dbdata.length}개의 게시물.</div>
+    <Div_all_table>
+      <ColumnCenter>
+        <Font_bold fontsize="17px"> 나의 게시물입니다.</Font_bold>
+      </ColumnCenter>
+      <Font13px_darkgray>
+        총 {dbdata.length}개의 게시물을 작성하였습니다.{" "}
+      </Font13px_darkgray>
 
-      <div className="div-full">
-        <div className="div-board">
-          {/* <p className="p-board">FORUM</p> */}
-         
-        </div>
+      <div>
+        <div>{/* <p className="p-board">FORUM</p> */}</div>
 
-        <div className="board_list">
+        <div>
           <List dbdata={dbdata}></List>{" "}
           {/*배열을 List.js에게 속성값으로 보낸다. */}
         </div>
       </div>
       {/* Board_write에 썼던 toast가 여기서 실행됨. */}
-    </>
+    </Div_all_table>
   );
 }
 
