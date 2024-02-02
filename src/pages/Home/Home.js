f839793528774bcbb8604f5b9ffa82e276929c7c
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2024-02-02 21:41:06 +0900

    Style: Delete unnecessary <div> tag

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,12 +3,16 @@ import React from "react";
 import { LeftContainer, RightContainer, Div_flex } from "../../styled-component/homeStyle";
 import NewRelease from "./NewRelease";
 import Intro from "./Intro";
+import ShopInfo from "./ShopInfo";
 
 function Home() {
   return (
     <Div_flex>
       <NewRelease/>
+      
       <Intro/>
+      
+      <ShopInfo/>
     </Div_flex>
   );
 }
