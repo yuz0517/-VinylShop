448ba9cc75a24c116ec9a9fcb2d794a41c229782
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2024-02-02 21:41:50 +0900

    Chore: Add Folder for administrator page

---
+++ src/pages/Administrator/AdminMain.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function AdminMain() {
+  return (
+    <>
+      <div>관리자 페이지</div>
+      <div>아이디</div><input type=""></input>
+      <div>비밀번호</div><input type=""></input>
+      <button>로그인</button>
+    </>
+  );
+}
+
+export default AdminMain;
