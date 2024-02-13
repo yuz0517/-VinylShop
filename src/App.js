ef251d3e238027b5171b68dd6e283866c26a4b5e
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2024-02-14 01:36:49 +0900

    Feat: Add condition to render Menubar only on admin pages

--- src/App.js
+++ src/App.js
@@ -22,8 +22,14 @@ import Orderdetail from "./pages/OrderSubmit/Orderdetail";
 import Point from "./pages/Mypage/Point";
 import AdminMain from "./pages/Administrator/AdminMain";
 import AdminLogin from "./pages/Administrator/AdminLogin";
+import UserSearchContainer from "./pages/Administrator/UserSearch/UserSearchContainer";
+import Menubar from "./pages/Administrator/Menubar";
+import { Div_flex } from "./styled-component/homeStyle";
 
 function App() {
+  const isAdminOpen = window.location.pathname.startsWith("/admin");
+  const isAdminSigninOpen =
+    window.location.pathname.startsWith("/signin/admin");
   const [isLoggedIn, setIsloggedIn] = useState(false);
   const [sessionUserid, setSessionUserid] = useState(""); //작성자 등록
   const [prevPagenation, setPrevpagenation] = useState(1);
@@ -40,29 +46,52 @@ function App() {
             setPrevpagenation,
           }}
         >
-          
-            <Navbar />
+          {isAdminOpen ? (
+            <>
+              <Menubar />
+              <Routes>
+                <Route path="/admin" element={<AdminMain />} />
 
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/admin" element={<AdminMain />} />
-            <Route path="/admin/login" element={<AdminLogin />} />
-            <Route path="/Mypage" element={<Mypage_Main />}></Route>
-            <Route path="/profile" element={<Mypage />} />
-            <Route path="/point" element={<Point />} />
-            <Route path="/vinyl" element={<Vinyl />}></Route>
-            <Route path="/vinyl/:id" element={<Item />} />
-            <Route path="/Signup" element={<Signup />} />
-            <Route path="/Board" element={<Board_Main />} />
-            <Route path="/Board-write" element={<Board_write />} />
-            <Route path="/Board-view/:no" element={<Board_View />} />
-            <Route path="/Board/update" element={<Board_Update />} />
-            <Route path="/Signin" element={<Signin />} />
-            <Route path="/Search/:no" element={<Search />} />
-            <Route path="/Search/mine" element={<Board_Mine />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/orderdetail" element={<Orderdetail />} />
-          </Routes>
+                <Route
+                  path="/admin/search/user"
+                  element={<UserSearchContainer />}
+                />
+              </Routes>
+            </>
+          ) : (
+            <>
+              {isAdminSigninOpen ? (
+                <>
+                <Routes>
+                  <Route path="/signin/admin" element={<AdminLogin/>}/>
+                </Routes>
+                </>
+              ) : (
+                <>
+                  <Navbar />
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+
+                    <Route path="/Mypage" element={<Mypage_Main />}></Route>
+                    <Route path="/profile" element={<Mypage />} />
+                    <Route path="/point" element={<Point />} />
+                    <Route path="/vinyl" element={<Vinyl />}></Route>
+                    <Route path="/vinyl/:id" element={<Item />} />
+                    <Route path="/Signup" element={<Signup />} />
+                    <Route path="/Board" element={<Board_Main />} />
+                    <Route path="/Board-write" element={<Board_write />} />
+                    <Route path="/Board-view/:no" element={<Board_View />} />
+                    <Route path="/Board/update" element={<Board_Update />} />
+                    <Route path="/Signin" element={<Signin />} />
+                    <Route path="/Search/:no" element={<Search />} />
+                    <Route path="/Search/mine" element={<Board_Mine />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/orderdetail" element={<Orderdetail />} />
+                  </Routes>
+                </>
+              )}
+            </>
+          )}
         </Context.Provider>
       </Router>
     </>
