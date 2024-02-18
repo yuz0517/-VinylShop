d84fb32d2854e03191b5e1be97e8f075a85796ae
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2024-02-18 23:56:18 +0900

    Chore: Add data for admin menubar

---
+++ src/pages/Administrator/MenubarData.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import * as AiIcons from "react-icons/ai";
+import * as BsIcons from "react-icons/bs";
+import * as VscIcons from "react-icons/vsc";
+import * as FaIcons from "react-icons/fa";
+const cName='admin-nav-text';
+export const MenubarData=[
+    {
+        title: 'Home',
+        path: '/admin',
+        icon:<AiIcons.AiFillHome />,
+        cName: "admin-nav-text",
+    },
+    {
+        title:'Users',
+        path: '/admin/search/user',
+        icon:<AiIcons.AiOutlineTeam />,
+        cName: "admin-nav-text",
+    },
+
+
+    
+]
