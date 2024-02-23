c325cc781d3085b1670d2c198504adfaf00e13d3
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2024-02-23 23:51:49 +0900

    restore firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);  
 const auth = getAuth(app);
 
 export { app, auth }
