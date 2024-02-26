ae2476f1270471bb6608d975afbe07c657744a05
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2024-02-26 23:52:25 +0900

    Feat: Add convertToKst function

--- src/Utils/Utils.js
+++ src/Utils/Utils.js
@@ -1,3 +1,16 @@
 export const numberWithCommas = (number) => {
-    return number.toLocaleString();
-  };
+  return number.toLocaleString();
+};
+
+export const convertToKst = (originDate) => {
+  var adjustDate = new Date(Date.parse(originDate) + 9 * 60 * 60000).toISOString(
+    Date(Date.parse(originDate) + 9 * 60 * 60000)
+  );
+ 
+  const year = adjustDate.slice(0, 4);
+  const month = adjustDate.slice(5, 7);
+  const day = adjustDate.slice(8, 10);
+  const hour = adjustDate.slice(11, 13);
+  const minute = adjustDate.slice(14,16)
+  return { year,month,day,hour,minute };
+};
