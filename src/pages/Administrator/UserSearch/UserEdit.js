c4fe52340b271d65522be3848f37d4e0bf698d44
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2024-02-27 00:00:17 +0900

    Feat: Develop partial implementation

---
+++ src/pages/Administrator/UserSearch/UserEdit.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import {
+  ColumnCenter,
+  Font_bold,
+  Font15px_darkgray,
+  Div_all,
+  Input_Rect_transparent,
+  Rect_transparent,
+  Select_Rect_transparent,
+  InputRectAddress,
+  Transparent_btn,
+  Radius_btn,
+  CustomMini,
+  Div_flex,
+} from "../../../styled-component/style.js";
+import { convertToKst } from "../../../Utils/Utils.js";
+export default function UserEdit() {
+  const location = useLocation();
+  const data = location.state;
+  const navigate = useNavigate();
+
+  const [nickName, setNickName] = useState(data.Nickname);
+  const [address, setAddress] = useState(data.Address);
+  const [address1, setAddress1] = useState(data.Address1);
+  const [role, setRole] = useState(data.admin);
+
+  const { year, month, day, hour, minute } = convertToKst(data.SignUpDate);
+
+  const onSaveClick = () => {
+
+  }
+  const onCancelClick = () => {
+    navigate('/admin/user');
+  }
+  console.log(data.Address);
+  return (
+    <Div_all>
+      <ColumnCenter>
+        <Font_bold fontSize="18px">Edit User Information</Font_bold>
+      </ColumnCenter>
+
+      <Font15px_darkgray>ID {`(email)`}</Font15px_darkgray>
+      <Rect_transparent> {data.userID} </Rect_transparent>
+      <Font15px_darkgray>고유번호</Font15px_darkgray>
+      <Rect_transparent> {data.PersonID} </Rect_transparent>
+      <Font15px_darkgray>가입일자</Font15px_darkgray>
+      <Rect_transparent>
+        {" "}
+        {year}-{month}-{day} {hour}:{minute}{" "}
+      </Rect_transparent>
+      <Font15px_darkgray>Nickname</Font15px_darkgray>
+      <Input_Rect_transparent
+        type="text"
+        defaultValue={nickName}
+        onChange={(e) => {
+          setNickName(e.target.value);
+        }}
+      />
+      <Font15px_darkgray>Address</Font15px_darkgray>
+      <Input_Rect_transparent
+        type="text"
+        defaultValue={data.Address}
+        onChange={(e) => {
+          setAddress(e.target.value);
+        }}
+      />
+      <Font15px_darkgray>상세주소</Font15px_darkgray>
+      <Input_Rect_transparent
+        type="text"
+        defaultValue={address1}
+        onChange={(e) => {
+          setAddress1(e.target.value);
+        }}
+      />
+      <Font15px_darkgray>Role</Font15px_darkgray>
+      <Select_Rect_transparent
+        onChange={(e) => {
+          setRole(e.target.value);
+        }}
+      >
+        <option value="1">Admin</option>
+        <option value="0">Customer</option>
+      </Select_Rect_transparent>
+      <Div_flex justifycontent="end">
+        <CustomMini
+          onClick={onSaveClick}
+          border="1px solid #000000"
+          height="33px"
+          width="60px"
+          fontSize="13px"
+          background="#fff000"
+          margin="10px 4px"
+        >
+          저장
+        </CustomMini>
+        <CustomMini
+          onClick={onCancelClick}
+          border="1px solid #000000"
+          height="33px"
+          width="60px"
+          fontSize="13px"
+          background="#dcdcdc"
+          margin="10px 0px 0px 0px"
+        >
+          취소
+        </CustomMini>
+      </Div_flex>
+    </Div_all>
+  );
+}
