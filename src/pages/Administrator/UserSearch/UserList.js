51053a549842370c3a023383dc21f028fb851545
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2024-03-12 22:57:39 +0900

    Feat: Separate the list as component

--- src/pages/Administrator/UserSearch/UserList.js
+++ src/pages/Administrator/UserSearch/UserList.js
@@ -8,6 +8,7 @@ import firebase from "firebase/compat/app";
 
 //import  { firebase } from "firebase/app"
 import Axios from "axios";
+import List from "./List";
 import {
   HiChevronUpDown,
   HiPencilSquare,
@@ -18,14 +19,11 @@ function UserList() {
   const [admin, setAdmin] = useState([]);
   const [selectedUser, setSelectedUser] = useState([]);
   const [selectedValue, setSelectedValue] = useState(0);
-  const [sortNO, setSortNO] = useState(0);
-  const [sortNAME, setSortNAME] = useState(0);
-  const [sortEMAIL, setSortEMAIL] = useState(0);
-  const [sortDATE, setSortDATE] = useState(0);
+  
   const [selectedArray, setSelectedArray] = useState([]);
   const [searchKeyword, setSearchKeyword] = useState("");
-  const [selectedListOption, setSelectedListOption] = useState("customer");
-  const [selectedSearchOption, setSelectedSearchOption] = useState("Email");
+  const [selectedListOption, setSelectedListOption] = useState(0);
+  const [selectedSearchOption, setSelectedSearchOption] = useState("userID");
   //let selectedArray = selectedValue === "admin" ? admin : customer;
 
   //selectedValue admin이면 admin배열을 넣고 아니면 customer 배열을 할당함.
@@ -35,7 +33,6 @@ function UserList() {
       .then((res) => {
         setCustomer([...res.data]);
         setSelectedArray([...res.data]);
-        console.log(res.data);
       })
       .catch((err) => {
         console.log(err.message);
@@ -51,110 +48,20 @@ function UserList() {
       });
   }, []);
 
-  const onSortNOClick = () => {
-    setSortNO(!sortNO);
-    sortNO
-      ? setSelectedArray((prevUser) => [
-          ...prevUser.sort((a, b) => b.PersonID - a.PersonID),
-        ])
-      : setSelectedArray((prevUser) => [
-          ...prevUser.sort((a, b) => a.PersonID - b.PersonID),
-        ]);
-  };
-  const onSortNAMEClick = () => {
-    console.log("눌림");
-    setSortNAME(!sortNAME);
-    sortNAME
-      ? setSelectedArray((prevUser) => [
-          ...prevUser.sort((a, b) => a.Nickname.localeCompare(b.Nickname)),
-        ])
-      : setSelectedArray((prevUser) => [
-          ...prevUser.sort((a, b) => b.Nickname.localeCompare(a.Nickname)),
-        ]);
-  };
-  const onSortEMAILClick = () => {
-    setSortEMAIL(!sortEMAIL);
-    sortEMAIL
-      ? setSelectedArray((prevUser) => [
-          ...prevUser.sort((a, b) => a.userID.localeCompare(b.userID)),
-        ])
-      : setSelectedArray((prevUser) => [
-          ...prevUser.sort((a, b) => b.userID.localeCompare(a.userID)),
-        ]);
-  };
-  const onSortDATEClick = () => {
-    setSortDATE(!sortDATE);
-    sortDATE
-      ? setSelectedArray((prevUser) => [
-          ...prevUser.sort((a, b) => a.SignUpDate.localeCompare(b.SignUpDate)),
-        ])
-      : setSelectedArray((prevUser) => [
-          ...prevUser.sort((a, b) => b.SignUpDate.localeCompare(a.SignUpDate)),
-        ]);
-  };
+  
   
 
-  const onDeleteClick = async (e) => {
-    const email = e.userID;
-    setSelectedUser(e);
+  
 
-    try {
-      console.log(selectedUser.userID);
-      const firebaseDeleteResponse = await fetch(
-        "http://localhost:8000/users/api/admin/user/delete",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email }),
-        }
-      );
-      const data = await firebaseDeleteResponse.json();
-
-      if (data.success) {
-        try {
-          const dbDeleteResponse = await Axios.delete(
-            "http://localhost:8000/api/admin/user/delete",
-            {
-              data: { id: email },
-            }
-          )
-            .then((res) => {
-              console.log(
-                selectedArray.filter((item) => item.userID !== email)
-              );
-              setSelectedArray(
-                selectedArray.filter((item) => item.userID !== email)
-              );
-              alert("삭제 완료되었습니다.");
-            })
-            .catch((err) => {
-              console.log("db에러");
-              console.log(err.message);
-            });
-        } catch (err) {
-          console.error(err.message);
-        }
-      } else {
-        console.error(data.error);
-      }
-    } catch (err) {
-      console.error(err.message);
-    }
-  };
-
-  const onEditClick = (e) => {
-    navigate("/admin/user/edit", { state: e });
-  };
+ 
 
   const onSelectChange = (e) => {
     if (e.target.value === "customer") {
       setSelectedArray(customer);
-      setSelectedListOption("customer");
+      setSelectedListOption("0");
     } else if (e.target.value === "admin") {
       setSelectedArray(admin);
-      setSelectedListOption("admin");
+      setSelectedListOption("1");
     }
   };
   const onSearchClick = () => {
@@ -162,8 +69,8 @@ function UserList() {
   };
   
   const onSearchSelectChange = (e) => {
-    (e.target.value === 'id') ? setSelectedSearchOption("id") :
-    (e.target.value === 'nickname') ? setSelectedSearchOption("nickname") : setSelectedSearchOption("email");
+    (e.target.value === 'id') ? setSelectedSearchOption("PersonID") :
+    (e.target.value === 'nickname') ? setSelectedSearchOption("Nickname") : setSelectedSearchOption("userID");
   }
 
   return (
@@ -182,51 +89,7 @@ function UserList() {
           <option value="admin">Admin</option>
         </select>
       </div>
-      <table>
-        <thead>
-          <tr>
-            <th onClick={onSortNOClick}>
-              NO <HiChevronUpDown onClick={onSortNOClick} />
-            </th>
-            <th onClick={onSortNAMEClick}>
-              NAME <HiChevronUpDown onClick={onSortNAMEClick} />
-            </th>
-            <th onClick={onSortEMAILClick}>
-              EMAIL <HiChevronUpDown onClick={onSortEMAILClick} />
-            </th>
-            <th onClick={onSortDATEClick}>
-              DATE <HiChevronUpDown onClick={onSortDATEClick} />
-            </th>
-            <th>OPTIONS</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {selectedArray.map((item) => {
-            var date_kst = new Date(
-              Date.parse(item.SignUpDate) + 9 * 60 * 60000
-            ).toISOString(Date(Date.parse(item.SignUpDate) + 9 * 60 * 60000));
-            var kst =
-              date_kst.slice(0, 4) +
-              "/" +
-              date_kst.slice(5, 7) +
-              "/" +
-              date_kst.slice(8, 10);
-            return (
-              <tr>
-                <td>{item.PersonID}</td>
-                <td>{item.Nickname}</td>
-                <td>{item.userID}</td>
-                <td>{kst}</td>
-                <td>
-                  <HiPencilSquare onClick={() => onEditClick(item)} />
-                  <HiOutlineTrash onClick={() => onDeleteClick(item)} />
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <List dataArray={selectedArray}/>
       {/* 
             <Pagination
                 // 현재 보고있는 페이지 
