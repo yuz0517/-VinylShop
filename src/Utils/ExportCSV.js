e45859bedc0c032794c107f62dcbfab65562281d
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2024-03-21 15:30:28 +0900

    Refactor: Uitilize convertToKst function for generating current date and time string

--- src/Utils/ExportCSV.js
+++ src/Utils/ExportCSV.js
@@ -1,33 +1,18 @@
 import React from "react";
 import { CSVLink } from "react-csv";
 import Button from "@mui/material/Button";
+import { convertToKst } from "./Utils";
 
 function ExportCSV(props) {
   //console.log((props.data).length)
   console.log(props.data.length);
-  const today = new Date();
-  const todayFull =
-    today.getFullYear() +
-    "" +
-    (today.getMonth() + 1) +
-    "" +
-    today.getDate() +
-    "" +
-    (today.getHours()+''.length === 1
-      ? 0 + (today.getHours() + "")
-      : today.getHours()) +
-    "" +
-    (today.getMinutes()+''.length === 1
-      ? 0 + (today.getMinutes() + "")
-      : today.getMinutes()) +
-    "" 
-
+  const today = convertToKst(new Date()).fullDate
   props.data.forEach((obj) => delete obj.withdrawal);
 
   return (
     <>
       {props.data.length > 0 ? (
-        <CSVLink filename={todayFull} data={props.data}>
+        <CSVLink filename={today} data={props.data}>
           <Button>CSV download</Button>
         </CSVLink>
       ) : (
