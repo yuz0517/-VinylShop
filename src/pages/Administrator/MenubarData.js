3a7784c9fb905354878d52a9b922c78465aa504f
YUZ 🧚‍♀️ <yuz0517yuz@gmail.com>
2024-04-15 23:47:13 +0900

    Feat: Add subscribe menu on the Admin page

--- src/pages/Administrator/MenubarData.js
+++ src/pages/Administrator/MenubarData.js
@@ -3,6 +3,8 @@ import * as AiIcons from "react-icons/ai";
 import * as BsIcons from "react-icons/bs";
 import * as VscIcons from "react-icons/vsc";
 import * as FaIcons from "react-icons/fa";
+import { HiOutlinePaperAirplane } from "react-icons/hi2";
+
 const cName='admin-nav-text';
 export const MenubarData=[
     {
@@ -15,6 +17,12 @@ export const MenubarData=[
         title:'Users',
         path: '/admin/user',
         icon:<AiIcons.AiOutlineTeam />,
+        cName: "admin-nav-text",
+    },
+    {
+        title:'Subscribe Users',
+        path: '/admin/subscribe',
+        icon:<HiOutlinePaperAirplane/>,
         cName: "admin-nav-text",
     },
 
